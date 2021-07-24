8950708c33472126e3af9a2950e0499d4bd1c4f7
melyman95 <mattlyman12@gmail.com>
2021-07-24 03:09:14 -0700

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCModelBinding.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public string ReleaseYear { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Rating { get; set; }
+
+        public int Length { get; set }
+
+    }
+}
